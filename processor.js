79370b5f1807097a10e85a755f4110d7315a380e
RoryEgan <roryegan1997@gmail.com>
2016-09-05 12:15:50 +0100

    New code added:
    The task is now a newer version that creates small images and maintains directory structure. README has been updated.

--- processor.js
+++ processor.js
@@ -1,71 +1,101 @@
 // Image processing
-  // ---------------------------------------------------------------------------
-  responsive_images: {
-    bigTask: {
-      options: {
-        engine: 'im',
-        sizes: [{
-          name: "big-images",
-          width: 1920,
-          quality: 70,
-        },]
+    // ---------------------------------------------------------------------------
+    responsive_images: {
+      smallTask: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: "small-images",
+            width: 240,
+            quality: 60,
+          },]
+        },
+        files: [{
+          cwd: '_source-images/',
+          src: ['**/*.{jpg,gif,png,jpeg}'],
+          custom_dest: 'assets/images/{%= name %}/{%= path %}/',
+          expand: true,
+        }]
       },
-      files: [{
-        cwd: '_source-images/big-images',
-        src: ['**/*.{jpg,gif,png}'],
-        custom_dest: 'assets/images/{%= name %}/',
-        expand: true,
-      }]
+      mediumTask: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: "medium-images",
+            width: 720,
+            quality: 60,
+          },]
+        },
+        files: [{
+          cwd: '_source-images/',
+          src: ['**/*.{jpg,gif,png,jpeg}'],
+          custom_dest: 'assets/images/{%= name %}/{%= path %}/',
+          expand: true,
+        }]
+      },
+      bigTask: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: "big-images",
+            width: 1920,
+            quality: 60,
+          },]
+        },
+        files: [{
+          cwd: '_source-images/',
+          src: ['**/*.{jpg,gif,png,jpeg}'],
+          custom_dest: 'assets/images/{%= name %}/{%= path %}/',
+          expand: true,
+        }]
+      },
     },
-    mediumTask: {
-      options: {
-        engine: 'im',
-        sizes: [{
-          name: "medium-images",
+    // Main Image task
+    // ---------------------------------------------------------------------------
+    resize_crop: {
+      smallTask: {
+        options: {
+          format: "{jpg,gif,png,jpeg}",
+          gravity: "center",
+          height: 160,
+          width: 240,
+          expand: true,
+        },
+        files: {
+          'assets/images/small-images': [
+            '_source-images/*.{jpg,gif,png,jpeg}',
+          ],
+        },
+      },
+      mediumTask: {
+        options: {
+          format: "{jpg,gif,png,jpeg}",
+          gravity: "center",
+          height: 460,
           width: 720,
-          quality: 70,
-        },]
+          expand: true,
+        },
+        files: {
+          'assets/images/medium-images': [
+            '_source-images/*.{jpg,gif,png,jpeg}',
+          ],
+        },
       },
-      files: [{
-        cwd: '_source-images/medium-images',
-        src: ['**/*.{jpg,gif,png}'],
-        custom_dest: 'assets/images/{%= name %}/',
-        expand: true,
-      }]
-    }
-  },
-  // Main Image task
-  // ---------------------------------------------------------------------------
-  resize_crop: {
-    mediumTask: {
-      options: {
-        format: "jpg",
-        gravity: "center",
-        height: 460,
-        width: 720,
-        expand: true,
-      },
-      files: {
-        'assets/images/medium-images': [
-          '_source-images/medium-images/*.{jpg,gif,png}',
-        ],
+      bigTask: {
+        options: {
+          format: "{jpg,gif,png,jpeg}",
+          gravity: "center",
+          height: 1064,
+          width: 1920,
+          expand: true,
+        },
+        files: {
+          'assets/images/big-images': [
+            '_source-images/*.{jpg,gif,png,jpeg}',
+          ],
+        },
       },
     },
-    bigTask: {
-      options: {
-        format: "jpg",
-        gravity: "center",
-        height: 1064,
-        width: 2000,
-        expand: true,
-      },
-      files: {
-        'assets/images/big-images': [
-          '_source-images/big-images/*.{jpg,gif,png}',
-        ],
-      },
-    },
-  },
 
 // ------------------------------------------------------------------------------------
 
